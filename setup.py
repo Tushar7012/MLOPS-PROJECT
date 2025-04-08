1401312796d31d5c509db83b8fef127dcba4fc75
Tushar <td220627@gmail.com>
2025-04-08 10:44:28 +0530

    Project Structure

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from typing import List
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+
+    """
+    The function is used return the list of requirements.
+
+    """
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        # Not going to use the \n in the requirements
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT == "-e .":
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+name = "mlproject",
+version = "0.0.1",
+author = "Tushar",
+author_email = "td220627@gmail.com",
+packages = find_packages(),
+inatall_requires = get_requirements("requirements.txt")
+)
